f94b2d5163c07e242be85ca8d32141a9799d2c20
azinoVo <azinovo22@gmail.com>
2019-05-17 11:56:11 -0500

    added form for adding smurf, debugging state

--- smurfs/src/components/AddSmurf.js
+++ smurfs/src/components/AddSmurf.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
 
 class AddSmurf extends Component {
+    constructor() {
+        super();
+        this.state = {
+            smurfInfo: {
+                name:'',
+                age:'',
+                height:''
+            }
+        }
+    }
+
+    handleChanges = event => {
+
+    }
+
+    addSmurf = event => {
+
+    }
+
+    
     render() {
         return (
             <div>
-                FORM HERE
+            <form onSubmit={this.addSmurf}>
+                    <input
+                        type="text"
+                        name='name'
+                        value={this.state.smurfInfo.name}
+                        onChange={this.handleChanges}
+                        placeholder="...name"
+                    />
+
+                    <input
+                        type="number"
+                        name='age'
+                        value={this.state.smurfInfo.age}
+                        onChange={this.handleChanges}
+                        placeholder="...age"
+                    />
+
+                    <input
+                        type="text"
+                        name='name'
+                        value={this.state.smurfInfo.height}
+                        onChange={this.handleChanges}
+                        placeholder="...height in cm"
+                    />
+                    <button>Add Smurf</button>
+                </form>
             </div>
         );
     }
